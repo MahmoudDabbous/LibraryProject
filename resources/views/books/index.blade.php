17ece4e1beca380d3a4333437b6f9edce988f130
MahmoudDabbous <mahmoud19728@gmail.com>
2022-08-03 23:56:13 +0200

    syntax error fixed

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Books')
 @section('content')
     @foreach ($books as $item)
-        <h3><a href="/book/{{ $item->id }}">{{ $item->title }}</a></h3>
+        <h3><a href="/books/{{ $item->id }}">{{ $item->title }}</a></h3>
         <p>{{ $item->description }}</p>
         <small>{{ $item->price }}</small>
         <hr>
