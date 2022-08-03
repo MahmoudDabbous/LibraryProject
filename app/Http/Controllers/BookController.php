436f1fcf25ab1297216fc24de2f97a2c9fd147fd
MahmoudDabbous <mahmoud19728@gmail.com>
2022-08-03 23:51:29 +0200

    Added select all and select one to BookController

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
     public function index()
     {
-        return 'This a book table';
+        $books = Book::orderBy('id', 'ASC')
+            ->paginate(5);
+        return view('books.index', compact('books'));
+    }
+    public function show($id)
+    {
+        $book = Book::findOrFail($id);
+        return view('books.show', compact('book'));
     }
 }
